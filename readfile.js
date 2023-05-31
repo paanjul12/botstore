d4d3c45f7d6ec46e5090db14409e9241ad7aa0fe
paanjul12 <124282756+paanjul12@users.noreply.github.com>
2023-05-31 13:28:11 +0700

    Add files via upload

---
+++ readfile.js
@@ -0,0 +1,36 @@
+/*
+  * DannTeam
+  * ig: @jkt48.danzz
+*/
+
+// Big Thanks To
+// - All Creator
+// - All Supporter
+// - All Donationer
+// - All Progammer
+// - All Team
+// - All Community
+// - All People
+// - God [ ! ]
+// - Family [ ! ]
+
+// Special Thanks To
+// - [ DannLonely ] > Pengembang dari Dann-MD
+// - [ NANS OFFICIAL ] > Promoter
+// - [ VynaChan ] > Promoter
+// - [ DannTeam ] > Pengembang semua fitur Dann-MD
+// - [ Arippanteq ] > Helper
+// - [ Skyline ] > Mengembangkan apikey dengan unlimited
+// - [ Kaito Saikyo ] > Moderators / Teams
+// - [ DannGanteng ] > Administrator
+
+// Jangan dihapus.
+// Berterimakasihlah jika masih ada script seperti begini
+// All Team menyanyangi kamu jika kamu menghargai script yang di buat oleh * DannLonely *
+// Kami sangat berterimakasih untuk kalian yang sudah mendukung dari Donasi, dan lain-lain.
+
+// Logger for thanks you
+console.log('Thanks To DannTeam')
+
+// Jum'at, 12/05/23
+// 00.00 WITA
